8acf7eda06098584af2952b20f01ccd082497bf0
Jason Humbert <jasonhumbert@outlook.com>
2022-02-21 22:05:04 +0000

    Add initial 'deposit' tests

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,4 +6,23 @@ describe Bank do
   it 'has an initial balance of 0' do
     expect(bank.balance).to eq 0
   end
+
+  describe '#deposit' do
+    context 'before the first deposit' do
+      it 'set the balance equal to the deposit' do
+        bank.deposit(1000)
+
+        expect(bank.balance).to eq 1000
+      end
+    end
+
+    context 'after an initial deposit has been made' do
+      it 'adds to the balance' do
+        bank.deposit(1000)
+        bank.deposit(2000)
+
+        expect(bank.balance).to eq 3000
+      end
+    end
+  end
 end
