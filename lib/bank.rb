e60f7e8f6fa5a5111de81d081aa96cd222fbafa1
Jason Humbert <jasonhumbert@outlook.com>
2022-02-22 12:18:59 +0000

    Add deposit, withdrawal and balance amounts to printed statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,19 +3,39 @@ class Bank
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+
+    @transactions << create_transaction(date: "", credit: amount, debit: "", balance: @balance)
   end
 
   def withdraw(amount)
     raise "Insufficient funds available" if @balance < amount
 
     @balance -= amount
+
+    @transactions << create_transaction(date: "", credit: "", debit: amount, balance: @balance)
   end
 
   def view_statement
     puts "date || credit || debit || balance"
+    
+    @transactions.each do |t|
+      puts "#{t[:date]} || #{t[:credit]} || #{t[:debit]} || #{t[:balance]}"
+    end
+  end
+
+  private
+
+  def create_transaction(date:, credit:, debit:, balance:)
+    transaction = {
+      date: date,
+      credit: credit,
+      debit: debit,
+      balance: balance
+    }
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,6 +3,8 @@ require 'bank'
 describe Bank do
   subject (:bank) { described_class.new }
 
+  let(:header) { "date || credit || debit || balance\n" }
+
   it 'has an initial balance of 0' do
     expect(bank.balance).to eq 0
   end
@@ -45,7 +47,24 @@ describe Bank do
   describe '#view_statement' do
     context 'when no transactions have been made' do
       it 'prints the statement header' do
-        expect { bank.view_statement }.to output("date || credit || debit || balance\n").to_stdout
+        expect { bank.view_statement }.to output(header).to_stdout
+      end
+    end
+
+    context 'when one or more transactions have been made' do
+      it 'prints the deposit amount' do
+        bank.deposit(1000)
+
+        expect { bank.view_statement }.to output(header + " || 1000 ||  || 1000\n").to_stdout
+      end
+
+      it 'prints the withdrawal amount' do
+        bank.deposit(1000)
+        bank.withdraw(200)
+        transaction_1 = " || 1000 ||  || 1000\n"
+        transaction_2 = " ||  || 200 || 800\n"
+
+        expect { bank.view_statement }.to output(header + transaction_1 + transaction_2).to_stdout
       end
     end
   end
